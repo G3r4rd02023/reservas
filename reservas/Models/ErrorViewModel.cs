02cf605f269350a3d7ba5bf4627c6be99ec601f5
Gerardo Lanza <glanza007@gmail.com>
2022-12-21 10:01:31 -0600

    create project

---
+++ reservas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reservas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
